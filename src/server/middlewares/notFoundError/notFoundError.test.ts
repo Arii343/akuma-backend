1e85454ca5c985e919052b59fae4f5b115da8960
Ariadna Cañizares <ariadnacs@hotmail.com>
2023-05-25 17:08:00 +0200

    Add notFoundError tests

---
+++ src/server/middlewares/notFoundError/notFoundError.test.ts
@@ -0,0 +1,25 @@
+import { type Request, type NextFunction, type Response } from "express";
+import notFoundError from "./notFoundError.js";
+import CustomError from "../../../CustomError/CustomError.js";
+
+describe("Given a notFoundError middleware", () => {
+  describe("When it receives a next function", () => {
+    const req = {};
+    const res = {};
+    const next: NextFunction = jest.fn();
+
+    test("Then it should be called", () => {
+      notFoundError(req as Request, res as Response, next);
+
+      expect(next).toHaveBeenCalled();
+    });
+
+    test("Then it should be called with CustomError", () => {
+      const error = new CustomError("Endpoint not found", 404);
+
+      notFoundError(req as Request, res as Response, next);
+
+      expect(next).toHaveBeenCalledWith(error);
+    });
+  });
+});
