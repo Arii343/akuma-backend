41ba98550d7f64b608afc93fbeac17faf56f973f
Ariadna Cañizares <ariadnacs@hotmail.com>
2023-05-25 13:17:11 +0200

    Add cors middleware

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,7 +1,16 @@
 import express from "express";
+import cors from "cors";
 import morgan from "morgan";
 
 const app = express();
+
+const allowedOrigins = process.env.APP_URL!;
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins,
+};
+
+app.use(cors(options));
 
 app.disable("x-powered-by");
 
