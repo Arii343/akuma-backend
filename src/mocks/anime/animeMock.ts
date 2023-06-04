c67455e4908f05f25ddc5aae2b8c10ced0c9b5b1
Ariadna Cañizares <ariadnacs@hotmail.com>
2023-06-04 17:20:25 +0200

    Add mocks/anime folder to save animes mock data for animeController test

---
+++ src/mocks/anime/animeMock.ts
@@ -0,0 +1,20 @@
+export const animesMock = [
+  {
+    image: "https://cdn.myanimelist.net/images/anime/1995/135164l.webp",
+    englishTitle: "The Rising of the Shield Hero Season 3",
+    japaneseTitle: "盾の勇者の成り上がり",
+    type: "TV",
+    source: "Light novel",
+    episodes: 3,
+    status: "Finished Airing",
+    duration: "23 min",
+    rating: "PG-13 - Teens 13 or older",
+    score: 5,
+    rank: 238,
+    popularity: 776,
+    synopsis: "Third season of Tate no Yuusha no Nariagari.",
+    releaseYear: 2023,
+    genres: ["Action", "Adventure", "Drama", "Fantasy"],
+    demographics: ["Seinen", "Shounen"],
+  },
+];
