13c877191bbb0129902c71557e4e829a1c446dc8
Ariadna Cañizares <ariadnacs@hotmail.com>
2023-06-12 23:54:36 +0200

    Update jwt expiration time from 1 day to 30 days

--- src/server/controllers/user/userController.ts
+++ src/server/controllers/user/userController.ts
@@ -26,7 +26,7 @@ const loginUser = async (
     };
 
     const token = jwt.sign(tokenPayload, process.env.JWT_SECRET!, {
-      expiresIn: "1d",
+      expiresIn: "30d",
     });
 
     res.status(200).json({ token });
