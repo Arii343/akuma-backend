d00143f0a3c025990154c46ad7793d4fa821745a
Ariadna Cañizares <ariadnacs@hotmail.com>
2023-06-12 23:47:26 +0200

    Add addAnime controller to animeRouter

--- src/server/routers/anime/animeRouter.ts
+++ src/server/routers/anime/animeRouter.ts
@@ -1,14 +1,24 @@
 import { Router } from "express";
 import {
+  addAnime,
   deleteAnime,
   getAnimes,
 } from "../../controllers/anime/animeController.js";
 import auth from "../../middlewares/auth/authMiddleware.js";
+import { validate } from "express-validation";
+import addAnimeSchema from "../../utils/schemas/animeSchema.js";
 
 const animeRouter = Router();
 
 animeRouter.get("/", getAnimes);
 
 animeRouter.delete("/:id", auth, deleteAnime);
+
+animeRouter.post(
+  "/",
+  auth,
+  validate(addAnimeSchema, {}, { abortEarly: false }),
+  addAnime
+);
 
 export default animeRouter;
